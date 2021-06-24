b6b377cb02e04b5de1b38973bed15fb2cb34293d
Vanessa Francis <VanFrancis@users.noreply.github.com>
2021-06-24 11:00:00 -0400

    Update remote_authentication_test.dart
    bugs correction null safety

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,23 +1,20 @@
-import 'dart:io';
-
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
 
-  RemoteAuthentication({@required this.httpClient, @required this.url});
+  RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void> auth() async {
+  Future<void>? auth() async {
     await httpClient.request(url: url);
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url});
+  Future<void> request({required String url});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
